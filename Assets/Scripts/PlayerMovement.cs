cf24f7bf7d145c508513b24738966aea77289777
David Kromann Helle <davidkrogaming@gmail.com>
2023-04-07 17:16:47 +0200

    Add platforms

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,17 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] int JumpForce = 5;
     Rigidbody rb;
 
+    private Vector3 camRotation;
+    private Transform cam;
+    private Vector3 moveDirection;
+
+    [Range(-45, -15)]
+    public int minAngle = -30;
+    [Range(30, 80)]
+    public int maxAngle = 45;
+    [Range(50, 500)]
+    public int sensitivity = 200;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,11 +35,23 @@ public class PlayerMovement : MonoBehaviour
         velocity.x = horizontalInput * MovementSpeed; 
         velocity.z = verticalInput * MovementSpeed;
         
-         if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump"))
         {
             velocity.y = JumpForce;
         }
 
         rb.velocity = velocity;
+
+        //Rotate();
     }
+    
+    //void Rotate()
+    //{
+    //    transform.Rotate(Vector3.up * sensitivity * Time.deltaTime * Input.GetAxis("Mouse X"));
+
+    //    camRotation.x -= Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+    //    camRotation.x = Mathf.Clamp(camRotation.x, minAngle, maxAngle);
+
+    //    cam.localEulerAngles = camRotation;
+    //}
 }
